62f52d9ad96efc69c09b317e46547beb89a20ca6
MOHAMMADNIBRAZN <85448180+MOHAMMADNIBRAZN@users.noreply.github.com>
2021-06-12 16:48:50 +0530

    Add files via upload

---
+++ ARMSTRONG.java
@@ -0,0 +1,25 @@
+package ARMSTRONG;
+
+public class ARMSTRONG {
+
+	public static void main(String[] args) {
+		
+		int number = 371 ,originalNumber,remainder,result = 0;
+		
+		originalNumber = number;
+		
+		while (originalNumber !=0)
+		{
+			remainder = originalNumber % 10;
+			result += Math.pow(remainder, 3);
+			originalNumber /=10;
+		}
+		if(result == number)
+			System.out.println(number + " is an armstrong number.");
+		else
+			System.out.println(number + "is not armstrong number ");
+			
+
+	}
+
+}
